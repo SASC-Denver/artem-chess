a2a5827854cf2a56609f1c81bdfe0d8ce08e3d26
artem-sasc-instructor <artem.v.shamsutdinov@gmail.com>
2020-08-19 17:09:16 -0600

    2 files modified

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-// alert('Hello World!'); 
+export function clicked() {
+  alert('chicked with function call');
+}
+
+
+// var tdElements = document.getElementsByTagName('TD');
+// for(let tdElement of tdElements) {
+//   tdElement.addEventListener('click', function (event) {
+//     const target: HTMLElement = event.target;
+//     target.classList.add("X")
+//     target.innerHTML = "X"
+//   })
+// }
