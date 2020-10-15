deca9e090e9f34e0cea991f26cc49edd38d80cdc
artem-sasc-instructor <artem.v.shamsutdinov@gmail.com>
2020-10-14 18:26:33 -0600

    1 file modified

--- index.ts
+++ index.ts
@@ -2,6 +2,31 @@
 var tdElements = document.getElementsByTagName('TD');
 var isO = false;
 
+var arrayOfNumbers = [1, 2, 3, 4, 5, 6];
+var sum = 0;
+
+var arrayOfLetters = ['a', 'b', 'c', 'd', 'e'];
+var arrayOfStrings = ['what ', 'a ', 'beatuful ', 'world ', '!'];
+
+var allLetersSoFar = '';
+for(var aLetter of arrayOfLetters) {
+  allLetersSoFar = allLetersSoFar + aLetter;
+  // alert('current letter is: ' + aLetter + ', all letters so far is: ' + allLetersSoFar);
+}
+var allLetersSoFar = '';
+for(var aLetter of arrayOfLetters) {
+  allLetersSoFar = allLetersSoFar + aLetter;
+  // alert('current letter is: ' + aLetter + ', all letters so far is: ' + allLetersSoFar);
+}
+
+// index is the position number - 1
+// alert('the 5th number in the array is: ' + arrayOfNumbers[4]);
+
+for(var aNumber of arrayOfNumbers) {
+  sum = sum + aNumber;
+  // alert('current number is: ' + aNumber + ', sum of all numbers so far is: ' + sum);
+}
+
 for(var tdElement of tdElements) {
   tdElement. addEventListener('click', handleClick);
 }
