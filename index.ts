5289e72ae115f5bef21791011fa9e2101bf83b2a
artem-sasc-instructor <artem.v.shamsutdinov@gmail.com>
2020-10-14 18:54:53 -0600

    1 file modified

--- index.ts
+++ index.ts
@@ -1,25 +1,26 @@
-
-var tdElements = document.getElementsByTagName('TD');
+var tdElements = document.getElementsByTagName("TD");
 var isO = false;
 
+// Copy from here
 var arrayOfNumbers = [1, 2, 3, 4, 5, 6];
 var sum = 0;
-for(var aNumber of arrayOfNumbers) {
+for (var aNumber of arrayOfNumbers) {
   sum = sum + aNumber;
   // alert('current number is: ' + aNumber + ', sum of all numbers so far is: ' + sum);
 }
+// Copy to here
 
-var arrayOfLetters = ['a', 'b', 'c', 'd', 'e'];
-var world = 'world'
-var arrayOfStrings = ['what ', 'a ', 'beatuful ', world, '!'];
+var arrayOfLetters = ["a", "b", "c", "d", "e"];
+var world = "world";
+var arrayOfStrings = ["what ", "a ", "beatuful ", world, "!"];
 
-var allStringsSoFar = '';
-for(var aString of arrayOfStrings) {
+var allStringsSoFar = "";
+for (var aString of arrayOfStrings) {
   allStringsSoFar = allStringsSoFar + aString;
   // alert('current string is: ' + aString + ', all strings so far is: ' + allStringsSoFar);
 }
-var allLetersSoFar = '';
-for(var aLetter of arrayOfLetters) {
+var allLetersSoFar = "";
+for (var aLetter of arrayOfLetters) {
   allLetersSoFar = allLetersSoFar + aLetter;
   // alert('current letter is: ' + aLetter + ', all letters so far is: ' + allLetersSoFar);
 }
@@ -27,22 +28,20 @@ for(var aLetter of arrayOfLetters) {
 // index is the position number - 1
 // alert('the 5th number in the array is: ' + arrayOfNumbers[4]);
 
-
-for(var tdElement of tdElements) {
-  tdElement. addEventListener('click', handleClick);
+for (var tdElement of tdElements) {
+  tdElement.addEventListener("click", handleClick);
 }
 
 function handleClick(event) {
   var target = event.target;
   if (isO) {
-    target.innerHTML = 'O';
+    target.innerHTML = "O";
     isO = false;
   } else {
-    target.innerHTML = 'X';
+    target.innerHTML = "X";
     isO = true;
   }
 }
-
 
 // var tdElements = document.getElementsByTagName('TD');
 // for(let tdElement of tdElements) {
