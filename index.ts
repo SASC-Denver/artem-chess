cb149a3d2edd46cd9c7bc791ac4698938dce0096
artem-sasc-instructor <artem.v.shamsutdinov@gmail.com>
2020-11-18 19:14:46 -0700

    2 files modified

--- index.ts
+++ index.ts
@@ -1,45 +1,6 @@
 var tdElements = document.getElementsByTagName("TD");
 var isO = false;
 
-
-
-
-
-
-
-
-var arrayOfAnything = [1, true, "a", "hello", { testProperty: false}, function () {alert("I'm a function");}];
-
-// alert("arrayOfAnything.length: " + arrayOfAnything.length);
-
-
-
-
-
-
-
-
-
-
-
-var arrayOfLetters = ["a", "b", "c", "d", "e"];
-var world = "world";
-var arrayOfStrings = ["what ", "a ", "beatuful ", world, "!"];
-
-var allStringsSoFar = "";
-for (var aString of arrayOfStrings) {
-  allStringsSoFar = allStringsSoFar + aString;
-  // alert('current string is: ' + aString + ', all strings so far is: ' + allStringsSoFar);
-}
-var allLetersSoFar = "";
-for (var aLetter of arrayOfLetters) {
-  allLetersSoFar = allLetersSoFar + aLetter;
-  // alert('current letter is: ' + aLetter + ', all letters so far is: ' + allLetersSoFar);
-}
-
-// index is the position number - 1
-// alert('the 5th number in the array is: ' + arrayOfNumbers[4]);
-
 for (var tdElement of tdElements) {
   tdElement.addEventListener("click", handleClick);
 }
