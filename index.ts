da9d74c93929453e1c3c6cbfd86719cdcb4049f2
artem-sasc-instructor <artem.v.shamsutdinov@gmail.com>
2020-11-22 15:09:41 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -1,16 +1,52 @@
+// Here we find all "td" elements on the screen
+// and assign them to the tdElements variable
 var tdElements = document.getElementsByTagName("TD");
+
+// Then we iterate over all of the tdElements and
+// for each "td" element assign it to a tdElement variable
 for (var tdElement of tdElements) {
+  // Adding an event listener allows the program to
+  // do something when a particular event happens
+
+  // Here we are adding an event listener for the
+  // "click" events.  This means tht when a user
+  // clicks on a <td></td> element the "handleClick"
+  // function will be called
   tdElement.addEventListener("click", handleClick);
 }
 
+// This is a boolean variable that determines
+// if an X or an O is shown on the screen in a particular
+// <td></td> element
 var isTheClickForAnO = false;
+
+
+// This is a function named "handleClick" that takes
+// one parameter named "event".  The event parameter
+// is passed to this function by the browser, because
+// this function handles events
 function handleClick(event) {
-  var target = event.target;
+
+  // Event has a target property, which contains the
+  // actual element that was clicked
+  var targetTd = event.target;
+
+  // If we should put down an "O" then execute the
+  // block that belongs to the if statement
   if (isTheClickForAnO) {
-    target.innerHTML = "O";
+    // Set the contents of the <td></td> to be an "O"
+    targetTd.innerHTML = "O";
+    // Set the boolean variable to the opposite value 
+    // (false) so that we place an "X" next time
     isTheClickForAnO = false;
-  } else {
-    target.innerHTML = "X";
+  } else
+  // If we should put down an "X" then execute the
+  // block that belongs to the else statement
+  {
+    // Set the contents of the <td></td> to be an "X"
+    targetTd.innerHTML = "X";
+    // Set the boolean variable to the opposite value
+    // (true) so that we place an "O" next time
     isTheClickForAnO = true;
   }
 }
