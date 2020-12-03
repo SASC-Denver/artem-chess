89c9ef9f31b5f40ddc7fa7e7bb308285f09c53df
artem-sasc-instructor <artem.v.shamsutdinov@gmail.com>
2020-12-02 19:18:46 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -2,6 +2,14 @@ import "./style.css";
 
 var tdElements = document.getElementsByTagName("TD");
 
+var board = [
+  ['X', null, 'Z'],
+  [null, 'O', null],
+  ['W', null, 'Y']
+];
+
+alert(board[0][0]);
+
 for (var tdElement of tdElements) {
   tdElement.addEventListener("click", handleClick);
 }
