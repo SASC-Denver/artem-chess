c681749855b6314dcceb3cf630c9cd122e98b406
artem-sasc-instructor <artem.v.shamsutdinov@gmail.com>
2020-12-16 18:35:25 -0700

    1 file modified, 3 added

--- index.ts
+++ index.ts
@@ -1,41 +1,6 @@
 import "./style.css";
+import "./extra.css";
+import { basicVar } from "./basic";
+import { extraVar } from "./extra";
 
-var tdElements = document.getElementsByTagName("TD");
-
-var board = [
-  //
-  ["X", null, null], //
-  ["X", "O", null], //
-  ["X", "O", null] //
-];
-
-if (board[2][2]) {
-  // alert(board[2][2]);
-}
-
-document.getElementById("resetButton").addEventListener("click", function() {
-  for (var tdElement of tdElements) {
-    tdElement.innerHTML = "";
-    tdElement.classList.remove("O");
-    tdElement.classList.remove("X");
-  }
-});
-
-for (var tdElement of tdElements) {
-  tdElement.addEventListener("click", handleClick);
-}
-
-var isTheClickForAnO = false;
-
-function handleClick(event) {
-  var targetTd = event.target;
-  if (isTheClickForAnO) {
-    targetTd.innerHTML = "O";
-    targetTd.classList.add("O");
-    isTheClickForAnO = false;
-  } else {
-    targetTd.innerHTML = "X";
-    targetTd.classList.add("X");
-    isTheClickForAnO = true;
-  }
-}
+console.log(basicVar + extraVar);
