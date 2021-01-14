18bcb7598e146fca7bdcf94310695f2a8b79d271
artem-sasc-instructor <artem.v.shamsutdinov@gmail.com>
2021-01-13 19:29:11 -0700

    1 file modified

--- basic.ts
+++ basic.ts
@@ -9,6 +9,41 @@ var board = [
   [null, null, null] // rowIndex="2"
 ];
 
+var winningConditions = [
+  [{ row: 0, column: 0}, { row: 1, column: 0}, { row: 2, column: 0}],
+  [{ row: 0, column: 0}, { row: 0, column: 1}, { row: 0, column: 2}],
+  [{ row: 0, column: 0}, { row: 1, column: 1}, { row: 2, column: 2}],
+];
+
+
+/**
+  [{ row: 0, column: 1}, { row: 1, column: 1}, { row: 2, column: 1}],
+  [{ row: 0, column: 2}, { row: 1, column: 2}, { row: 2, column: 2}],
+
+
+  [{ row: 0, column: 0}, { row: 1, column: 0}, { row: 2, column: 0}],
+  [{ row: 0, column: 1}, { row: 1, column: 1}, { row: 2, column: 1}],
+  [{ row: 0, column: 2}, { row: 1, column: 2}, { row: 2, column: 2}],
+
+  [{ row: 0, column: 0}, { row: 1, column: 1}, { row: 2, column: 2}],
+  [{ row: 0, column: 0}, { row: 1, column: 1}, { row: 2, column: 2}],
+  [{ row: 0, column: 0}, { row: 1, column: 1}, { row: 2, column: 2}],
+  [{ row: 0, column: 0}, { row: 1, column: 1}, { row: 2, column: 2}],
+  [{ row: 0, column: 0}, { row: 1, column: 1}, { row: 2, column: 2}],
+  [{ row: 0, column: 0}, { row: 1, column: 1}, { row: 2, column: 2}],
+  [{ row: 0, column: 0}, { row: 1, column: 1}, { row: 2, column: 2}],
+  [{ row: 0, column: 0}, { row: 1, column: 1}, { row: 2, column: 2}],
+  [{ row: 0, column: 0}, { row: 1, column: 1}, { row: 2, column: 2}],
+  [{ row: 0, column: 0}, { row: 1, column: 1}, { row: 2, column: 2}],
+  [{ row: 0, column: 0}, { row: 1, column: 1}, { row: 2, column: 2}],
+  [{ row: 0, column: 0}, { row: 1, column: 1}, { row: 2, column: 2}],
+];
+*/
+function whoWon() {
+
+  // return 'X' 'O' or null
+}
+
 for (var tdElement of tdElements) {
   tdElement.addEventListener("click", handleClick);
 }
